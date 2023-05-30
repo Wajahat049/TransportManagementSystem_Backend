34b57480aff5ffffbdd113b22e9ca8dad8df4b13
Aleezah <aleezahikram1429@gmail.com>
2023-05-30 17:00:25 +0500

    Update equipment.schema.js

--- schemas/equipment.schema.js
+++ schemas/equipment.schema.js
@@ -32,32 +32,7 @@ export const equipmentSchema = z.object({
     required_error: "asset_type is required",
   }),
 
-  trailer_type: z.string({
-    required_error: "trailer_type is required",
-  }),
-  trailer_size: z.string({
-    required_error: "trailer_size is required",
-  }),
-  reefer_hours: z.time({
-    required_error: "reefer_hours is required",
-  }),
-
-  emergency_contact_no: z.number({}).optional().nullable(),
-  emergency_contact_name: z.string({}).optional().nullable(),
-  company_name: z.string({}).optional().nullable(),
-  HST_no: z.string({}).optional().nullable(),
-  pay_HST: z.string({}).optional().nullable(),
-  pay_by: z.string({}).optional().nullable(),
-  salary: z.number({}).optional().nullable(),
-  payment_frequency: z.string({}).optional().nullable(),
-  hourly_rate: z.number({}).optional().nullable(),
-  mileage_rate: z.number({}).optional().nullable(),
-  last_pay_period: z.string({}).optional().nullable(),
-  last_payment_date: z.coerce.date({}).optional().nullable(),
-  gross_payroll: z.number({}).optional().nullable(),
-  cash_advance_balance: z.number({}).optional().nullable(),
-  monthly_deductions: z.number({}).optional().nullable(),
-  license: z.string({}).optional().nullable(),
-  passport: z.string({}).optional().nullable(),
-  other_documents: z.string({}).optional().nullable(),
+  reefer_hours: z.number({}).optional().nullable(),
+  trailer_size: z.string({}).optional().nullable(),
+  trailer_type: z.string({}).optional().nullable(),
 });
